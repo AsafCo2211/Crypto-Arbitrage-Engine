5f9ae5c653ca4f4b115f021d4138e4a846a7800c
Asaf_Cohen <asaf221100@gmail.com>
2026-03-07 13:09:26 +0200

    chore: Initial project setup with CMake and standard directories

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "[SYSTEM] Crypto Arbitrage Engine Initialized." << std::endl;
+    std::cout << "[SYSTEM] Ready to burn some CPU cycles!" << std::endl;
+    
+    return 0;
+}
